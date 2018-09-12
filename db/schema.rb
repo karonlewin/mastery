b0052dd8acdfc77b2263909c6fed4180d3ff2b1e
Karon Lewin <karonlewin@gmail.com>
2018-09-12 18:03:02 -0300

    Changing the date format of scheduled_at, adding some Gems and testing rendering the calendar

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180912150317) do
+ActiveRecord::Schema.define(version: 20180912204259) do
 
   create_table "appointments", force: :cascade do |t|
     t.string "client"
-    t.datetime "scheduled_at"
+    t.time "scheduled_at"
     t.text "observations"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/factories/appointment.rb
+++ spec/factories/appointment.rb
@@ -1,7 +1,9 @@
+require 'faker'
+
 FactoryBot.define do
   factory :appointment do
     client { 'John' }
-    scheduled_at  { DateTime.new }
-    observations { 'Full body dragon tattoo '}
+    scheduled_at  { Time.new + rand(100000) }
+    observations { Faker::Pokemon.name + ' tattoo'}
   end
 end
