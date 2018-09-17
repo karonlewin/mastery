d217414295db09d6808df6a47ab8814830149e30
Karon Lewin <karonlewin@gmail.com>
2018-09-17 17:55:22 -0300

    Adding a session helper to make easier to login while testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,10 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
